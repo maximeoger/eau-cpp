3875735e5ec3e5bf5efae724b7e283f282be14e3
maximeoger <maximeoger93@proton.me>
2023-05-22 18:17:53 +0200

    eau04: suite de Fibonacci

---
+++ eau04.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+bool isNumber(string str) {
+  for(int i=0; i<str.length(); i++) {
+    if(isdigit(str[i]) == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void checkArgs(int argc, char* argv[]) {
+
+  if(argc == 1) {
+    throw invalid_argument("Veuillez entrer un nombre.");
+  }
+
+  bool moreThanOneArgument = argc > 2;
+  bool argumentIsNegativeOrNotInteger = false == isNumber(string(argv[1]));
+
+  if(argumentIsNegativeOrNotInteger) {
+    throw invalid_argument("Veuillez entrer un nombre entier.");
+  }
+
+  if(moreThanOneArgument) {
+    throw invalid_argument("Veuillez entre un seul nombre.");
+  }
+}
+
+int main (int argc, char* argv[]) {
+  try {
+    checkArgs(argc, argv);
+
+    int input = atoi(argv[1]);
+
+    if(input == 0) {
+      cout << 1 << "\n";
+    }
+
+    int _temp, 
+        i = 2, 
+        current = 1, 
+        last = 0;
+    
+    while(input > current) {
+      // stocker la valeur courante temporairement
+      _temp = current;
+      // la valeur courante est la somme des valeurs courante et ancienne
+      current = last + current;
+      // qui sera utilisée comme valeur précédente au prochain tour
+      last = _temp;
+      i++;
+    }
+
+    if(input!=current) {
+      throw invalid_argument("Le paramètre d\'entrée n'appartiens pas à la suite de Fibonacci.");
+    }
+
+    cout << i << "\n"; 
+
+    return 0;
+ 
+  } catch(const invalid_argument& e) {
+    cerr << "erreur: " << e.what() << "\n";
+    cerr << -1 << "\n";
+
+    return -1;
+  }
+}
